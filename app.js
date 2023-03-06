84b43ee9c3142b3e1773845f571cc9bb7950c459
sabantuchoudhury <shanthi.abantu@gmail.com>
2023-03-06 18:15:57 +0000

    fixed katakana switch

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const draggableListItems = document.querySelectorAll('.draggable-list li');
+// let draggableListItems = document.querySelectorAll('.draggable-list li');
 const endMessage = document.getElementById('endMessage');
 const restartBtn = document.getElementById('restart');
-const stopBtn = document.getElementById('hira-kata-switch');
+const switchBtn = document.getElementById('hira-kata-switch');
 const timerText = document.getElementById('timer');
-
 const timerMs = document.getElementById('timerMiliseconds');
 const timerS = document.getElementById('timerSeconds');
 const timerM = document.getElementById('timerMinutes');
@@ -11,27 +10,30 @@ const timerM = document.getElementById('timerMinutes');
 let selectedID;
 let dropID;
 let correctCounter = 0;
-
 let timer = true;
 let timerMinutes = 00;
 let timerSeconds = 00;
 let timerMiliseconds = 00;
 
+let hiraganaOn = true;
 const hiragana = ['あ','い','う','え','お','か','き','く','け','こ','さ','し','す','せ','そ','た','ち','つ','て','と','な','に','ぬ','ね','の','は','ひ','ふ','へ','ほ','ま','み','む','め','も','や','ゆ','よ','ら','り','る','れ','ろ','わ','を','ん'];
 const katakana = ['ア','イ','ウ','エ','オ','カ','キ','ク','ケ','コ','サ','シ','ス','セ','ソ','タ','チ','ツ','テ','ト','ナ','ニ','ヌ','ネ','ノ','ハ','ヒ','フ','ヘ','ホ','マ','ミ','ム','メ','モ','ヤ','ユ','ヨ','ラ','リ','ル','レ','ロ','ワ','ヲ','ン'];
+const english = ['a','i','u','e','o','ka','ki','ku','ke','ko','sa','shi','su','se','so','ta','chi','tsu','te','to','na','ni','nu','ne','no','ha','hi','fu','he','ho','ma','mi','mu','me','mo','ya','yu','yo','ra','ri','ru','re','ro','wa','wo','n']
 
 
 createOptions();
-draggableListItems.forEach((item) => {
-    console.log(item.id);
-});
 startTimer();
 addEventListeners();
 
 
 // MAKE LIST FOR OPTIONS:
 function createOptions() {
-    let array = katakana;
+    let array = hiragana
+    if (hiraganaOn == true) {
+        array = hiragana;
+    } else {
+        array = katakana;
+    }
     let ul = document.createElement('ul');
     ul.setAttribute('id', 'draggable-options')
     ul.classList.add('draggable-list');
@@ -46,8 +48,13 @@ function createOptions() {
         li.setAttribute('id', ('j' + x));
         ul.appendChild(li);
         li = document.createElement('li');
+
+        console.log(li.id);
+        
         x++;
     });
+
+    draggableListItems = document.querySelectorAll('.draggable-list li');
 }
 
 
@@ -107,7 +114,7 @@ function addEventListeners() {
 
 
 // STOPCLOCK:
-restartBtn.addEventListener('click', function() {
+function restartGame() {
     timer = false;
 
     timerMinutes = 0;
@@ -120,14 +127,38 @@ restartBtn.addEventListener('click', function() {
     correctCounter = 0;
     timer = true;
 
-    location.reload();
+    for (let i = 1; i < 47; i++) {
+        document.getElementById('e' + i).textContent = english[i-1];
+        document.getElementById('e' + i).style.backgroundColor = '#f0ffff';
+        document.getElementById('e' + i).style.borderBlockStyle = 'inset';
+    }
+
+    let ul = document.getElementById("draggable-options");
+    ul.remove();
+    createOptions();
+    startTimer();
+    addEventListeners();
+}
+
+restartBtn.addEventListener('click', function() {
+    restartGame();
 });
 
-stopBtn.addEventListener('click', function() {
-    timer = false;
+switchBtn.addEventListener('click', function() {
+    let value = switchBtn.value;
+    if (value === "hiragana") {
+        switchBtn.value = 'katakana';
+        hiraganaOn = false;
+    } else {
+        switchBtn.value = 'hiragana';
+        hiraganaOn = true;
+    }
+    restartGame();
 });
 
 function startTimer() {
+
+    
     if (timer) {
         timerText.style.color = '#000000';
 
